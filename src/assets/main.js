c44184e319cdedbbb525350937428efd46d250dd
Tushara00 <ahnafmon6@gmail.com>
2024-02-19 21:55:46 +0600

    hamburger small issue fix

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,20 +6,8 @@ bar.addEventListener("click", function(){
 menu.classList.toggle ("menu-active");
 })
 
-const menuLists = document.querySelectorAll(".active");
 
-menuLists.forEach(function (menuList) {
-    menuList.addEventListener("click", function () {
-        document.querySelector(".active-list")?.classList.remove("active-list");
-       
-        menuList.classList.add("active-list");
-    });
-});
 
-const imagContainer = document.querySelector(".image-box-container");
-imagContainer.addEventListener("click", function(){
-    document.querySelector(".drop-down-menu-show")?. classList.remove("drop-down-menu-show");  
-})
 
 
 
